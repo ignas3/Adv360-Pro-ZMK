133963545b205c005745442105d344e20ac8e07e
Ignas <ignas.ziberkas@gmail.com>
2025-07-02 19:47:46 +0300

    Add key positions.

---
+++ config/adv360_keypos.h
@@ -0,0 +1,100 @@
+// left number row
+#define LN0 0
+#define LN1 1
+#define LN2 2
+#define LN3 3
+#define LN4 4
+#define LN5 5
+#define LN6 6
+
+// left top row
+#define LT0 14
+#define LT1 15
+#define LT2 16
+#define LT3 17
+#define LT4 18
+#define LT5 19
+#define LT6 20
+
+// left middle row
+#define LM0 28
+#define LM1 29
+#define LM2 30
+#define LM3 31
+#define LM4 32
+#define LM5 33
+#define LM6 34
+
+// left bottom row
+#define LB0 46
+#define LB1 47
+#define LB2 48
+#define LB3 49
+#define LB4 50
+#define LB5 51
+
+// left control row
+#define LC0 60
+#define LC1 61
+#define LC2 62
+#define LC3 63
+#define LC4 64
+
+// right number row
+#define RN0 7
+#define RN1 8
+#define RN2 9
+#define RN3 10
+#define RN4 11
+#define RN5 12
+#define RN6 13
+
+// right top row
+#define RT0 21
+#define RT1 22
+#define RT2 23
+#define RT3 24
+#define RT4 25
+#define RT5 26
+#define RT6 27
+
+// right middle row
+#define RM0 39
+#define RM1 40
+#define RM2 41
+#define RM3 42
+#define RM4 43
+#define RM5 44
+#define RM6 45
+
+// right bottom row
+#define RB0 54
+#define RB1 55
+#define RB2 56
+#define RB3 57
+#define RB4 58
+#define RB5 59
+
+// right control row
+#define RC0 71
+#define RC1 72
+#define RC2 73
+#define RC3 74
+#define RC4 75
+
+// left thumb keys
+#define LH0 35
+#define LH1 36
+#define LH2 52
+#define LH3 65
+#define LH4 66
+#define LH5 67
+
+// right thumb keys
+#define RH0 37
+#define RH1 38
+#define RH2 53
+#define RH3 68
+#define RH4 69
+#define RH5 70
+
